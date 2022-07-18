47316e8411d2e2173ae70182d5cedd40eb9f9e94
mikad74 <mika.doorenbosch@gmail.com>
2022-07-18 10:02:51 +0200

    parser passed tests

--- js/parser.js
+++ js/parser.js
@@ -1,5 +1,23 @@
-function parser(str) {
-  console.log(str)
-  return str
+function getParseType(char) {
+  if (char.match(/\d/)) return "number";
+  if (char.match(/[\+\-\*\/]/)) return "operator";
+  if (char.match(/[\(\)]/)) return "bracket";
+  return "function";
 }
 
+function parser(str) {
+  let parsedString = [];
+  let workingString = str.replace(/\s/g,"");
+  while (workingString.length >= 1) {
+    let currentStringParse = "";
+    const currentParseType = getParseType(workingString.slice(0, 1));
+    let i = 0;
+    while (currentParseType === getParseType(workingString.slice(i, i + 1))) {
+      currentStringParse += workingString.slice(i, i + 1);
+      i++
+    }
+    workingString = workingString.slice(i);
+    parsedString.push(currentStringParse);
+  }
+  return parsedString;
+}
--- js/parserTestable.spec.js
+++ js/parserTestable.spec.js
@@ -1,21 +1,21 @@
-import  parser from './parserTestable'
+const parser = require('./parserTestable')
 
 test("can parse simple expressions", () => {
-  expect(parser("95+12")).toBe(["95", "+", "12"]);
+  expect(parser("95+12")).toEqual(["95", "+", "12"]);
 });
 
 test("can parse expressions with multiple operators", () => {
-  expect(parser("95+12-13/4")).toBe(["95", "+", "12", "-", "13", "/", "4"]);
+  expect(parser("95+12-13/4")).toEqual(["95", "+", "12", "-", "13", "/", "4"]);
 });
 
 test("can parse with brackets", () => {
-  expect(parsers("(12+15)/10")).toBe(["(", "12", "+", "15", ")", "/", "10"]);
+  expect(parser("(12+15)/10")).toEqual(["(", "12", "+", "15", ")", "/", "10"]);
 });
 
 test("can parse functions without brackets", () => {
-  expect(parser("ln15")).toBe(["ln", "15"]);
+  expect(parser("ln15")).toEqual(["ln", "15"]);
 });
 
 test("can parse functions with brackets", () => {
-  expect(parser("ln(15) + 12")).toBe(["ln", "(", "15", ")", "+", "12"]);
+  expect(parser("ln(15) + 12")).toEqual(["ln", "(", "15", ")", "+", "12"]);
 });
